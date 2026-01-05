f1a767791ec1f5611a33a6b136c820c4aabb2012
Skylar Wolf <MrFastZombie@users.noreply.github.com>
2026-01-05 01:57:53 -0600

    First release

---
+++ README.md
@@ -0,0 +1,74 @@
+# Rimworld ContentFinder Optmiezr
+
+This is an experimental patch for the ContentFinder\<Texture2d>.Get() method in Rimworld.
+
+## Why?
+
+In my modlist (over 1000 mods), anything that calls ContentFinder\<Texture2d>.Get() repeatedly tends to use a lot of resources due to this method being fairly expensive when there are a lot of textures to search through. So I have introduced a cache to optimize these calls.
+
+## Should I use this?
+
+I don't expect many issues with using this mod, but I do not have a comprehensive understanding of the game's code so I will not make gurantees. In testing, I have found no issues.
+
+## Installation
+
+### Manual
+
+Clone this repository into the `Mods` folder in your Rimworld install directory.
+
+(Or you can click the green `Code` button above and press `Download Zip` and extract the zip to the folder instead.)
+
+Default folder locations (Info taken from <https://rimworldwiki.com/wiki/Modding_Tutorials/Mod_Folder_Structure>):
+
+1. Windows: `C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods`
+2. Mac: `~/Library/Application Support/Steam/steamapps/common/RimWorld/RimWorldMac.app/Mods`
+3. Linux (Steam): `~/.steam/steam/steamapps/common/RimWorld/Mods`
+4. Linux (GOG): `/home/<user>/GOG/Games/RimWorld/game/Mods/`
+
+Note that these locations may be different depending on whether you used a Steam library on another drive, used GOG or changed the GOG install directory.
+
+### Rimsort (Recommended!)
+
+In Rimsort's top bar, select `Download -> Add Git Mod` and put the URL of this repository in the text prompt that comes up.
+
+Once it downloads, press `Refresh` near the bottom to make the mod show in the left pane and drag it to the right pane.
+
+You can update the mod by right clicking it and selected `Miscellaneous options -> Update mod with git`.
+
+You may get a warning that this mod lacks a publish field ID. If this happens, select the mod and press add to ignore list.
+
+## Does it work?
+
+The test results below will show that this mod succeeds in its goal of improving FPS. Data was collecting using MSI Afterburner's benchmark feature.
+
+The tests below were run with my modlist of over 1000 mods and over 54,000 textures added from the mods.
+
+### Test 1: No patch, My mid-game save, Almost whole map in view and unpaused. 8 Pawns. 1 minute of data collection
+
+```txt
+05-01-2026, 01:05:57 RimWorldWin64.exe benchmark completed, 2375 frames rendered in 62.625 s
+                     Average framerate  :   37.9 FPS
+                     Minimum framerate  :   20.3 FPS
+                     Maximum framerate  :   58.8 FPS
+                     1% low framerate   :    8.2 FPS
+                     0.1% low framerate :    6.6 FPS
+```
+
+### Test 2: Patch enabled, My mid-game save, Almost whole map in view and unpaused. 8 Pawns. 1 minute of data collection
+
+```txt
+05-01-2026, 01:04:28 RimWorldWin64.exe benchmark completed, 4316 frames rendered in 62.265 s
+                     Average framerate  :   69.3 FPS
+                     Minimum framerate  :   49.8 FPS
+                     Maximum framerate  :   75.1 FPS
+                     1% low framerate   :   28.9 FPS
+                     0.1% low framerate :    8.0 FPS
+```
+
+This test shows a performance improvement of 80%!
+
+### Conclusion
+
+This patch will give significant improvement to heavy mod lists that have a huge amount of textures.
+
+While I have not specifically tested this case, I expect that light mod lists will see little improvement.
---
+++ Source/Main.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Sound;
+using Verse.Noise;
+using Verse.Grammar;
+using RimWorld;
+using RimWorld.Planet;
+
+using System.Reflection;
+using HarmonyLib;
+
+namespace HARCheckMaskShaderPatch
+{
+    public class ContentFinderOptimizerSettings : ModSettings
+    {
+        public bool enabled = true;
+        public int tickLifetime = 60;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref enabled, "enabled", true);
+            Scribe_Values.Look(ref tickLifetime, "tickLifetime", 60);
+            base.ExposeData();
+        }
+    }
+
+    public class ContentFinderOptimizerMod : Mod
+    {
+        string intBuffer;
+        public static ContentFinderOptimizerSettings settings;
+        public ContentFinderOptimizerMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<ContentFinderOptimizerSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("ContentFinderOptimizer.Enabled".Translate(), ref settings.enabled, tooltip: "EnableDesc".Translate());
+            listingStandard.Label("ContentFinderOptimizer.Label".Translate());
+            listingStandard.SubLabel("ContentFinderOptimizer.SubLabel".Translate(), 100f);
+            listingStandard.IntEntry(ref settings.tickLifetime, ref intBuffer, min: 1);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ContentFinderOptimizer.ModName".Translate();
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Start
+    {
+        static Start()
+        {
+            Log.Message("Starting ContentFinder Optimizer!");
+
+            Harmony harmony = new Harmony("ContentFinder Optimizer");
+            harmony.PatchAll( Assembly.GetExecutingAssembly() );
+        }
+    }
+    
+    [HarmonyPatch(typeof(Verse.ContentFinder<Texture2D>), "Get", new Type[] {typeof(string), typeof(bool)})]
+    public static class CheckMaskShader_Patch
+    {
+        private static Dictionary<string, Texture2D> textureCache = new Dictionary<string, Texture2D>();
+        private static Dictionary<string, int> textureTick = new Dictionary<string, int>();
+
+        public static bool Prefix(ref Texture2D __result, ref string itemPath, ref bool reportFailure)
+        {
+            int tick = GenTicks.TicksGame;
+            int tickLimit = LoadedModManager.GetMod<ContentFinderOptimizerMod>().GetSettings<ContentFinderOptimizerSettings>().tickLifetime;
+            bool enabled = LoadedModManager.GetMod<ContentFinderOptimizerMod>().GetSettings<ContentFinderOptimizerSettings>().enabled;
+            
+            if(enabled == false) return true;
+            
+            if(textureCache.ContainsKey(itemPath))
+            {
+                if(textureTick.ContainsKey(itemPath))
+                {
+                    //Log.Message("Tick limit is " + tickLimit);
+                    if(tick - textureTick[itemPath] >= tickLimit)
+                    {
+                        textureTick.Remove(itemPath);
+                        textureCache.Remove(itemPath);
+                        //Log.Message("Removed texture from cache: " + itemPath);
+                        return true;
+                    }
+                }
+                __result = textureCache[itemPath];
+                return false;
+            }
+            
+            return true;
+        }
+
+        static void Postfix(ref Texture2D __result, ref string itemPath)
+        {
+            bool enabled = LoadedModManager.GetMod<ContentFinderOptimizerMod>().GetSettings<ContentFinderOptimizerSettings>().enabled;
+            
+            if(enabled == false) return;
+            
+            if(textureCache.ContainsKey(itemPath) == false)
+            {
+                    //Log.Message("Adding texture to cache: " + itemPath);
+                    textureCache.Add(itemPath, __result);
+                    textureTick.Add(itemPath, GenTicks.TicksGame);
+            }
+        }
+    }
+}
